8367fa27bbd6f82960e308727bb9daa7526b6d3a
taar1 <taar1@d2b1d87f-eb27-4b14-ba05-7084dce0a13f>
2015-06-06 21:05:22 +0000

    Fixed the click listener Added CheatDatabaseApplication + added Event listener to the application (moving away from the activity)

--- trunk/app/src/main/java/com/cheatdatabase/adapters/SystemsRecycleListViewAdapter.java
+++ trunk/app/src/main/java/com/cheatdatabase/adapters/SystemsRecycleListViewAdapter.java
@@ -5,11 +5,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.cheatdatabase.CheatDatabaseApplication;
 import com.cheatdatabase.R;
 import com.cheatdatabase.businessobjects.SystemPlatform;
+import com.cheatdatabase.events.SystemListRecyclerViewClickEvent;
 import com.cheatdatabase.helpers.Konstanten;
+import com.cheatdatabase.helpers.Reachability;
 import com.cheatdatabase.helpers.Tools;
 
 import java.util.ArrayList;
@@ -20,22 +24,38 @@ public class SystemsRecycleListViewAdapter extends RecyclerView.Adapter<SystemsR
     private Typeface latoFontLight;
     private SystemPlatform systemObj;
 
+    private static final String TAG = SystemsRecycleListViewAdapter.class.getSimpleName();
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         public TextView mSystemName;
         public TextView mSubtitle;
+        public LinearLayout mLinearLayout;
+        public IMyViewHolderClicks mListener;
 
-        public ViewHolder(View v) {
+        public ViewHolder(View v, IMyViewHolderClicks listener) {
             super(v);
+            mListener = listener;
 
+            mLinearLayout = (LinearLayout) v.findViewById(R.id.ll);
             mSystemName = (TextView) v.findViewById(R.id.system_name);
             mSubtitle = (TextView) v.findViewById(R.id.subtitle);
+
+            v.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            mListener.onSystemClick(this);
+        }
+
+    }
+
+    public static interface IMyViewHolderClicks {
+        public void onSystemClick(SystemsRecycleListViewAdapter.ViewHolder caller);
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
@@ -45,19 +65,30 @@ public class SystemsRecycleListViewAdapter extends RecyclerView.Adapter<SystemsR
 
     // Create new views (invoked by the layout manager)
     @Override
-    public SystemsRecycleListViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public SystemsRecycleListViewAdapter.ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
 
         latoFontBold = Tools.getFont(parent.getContext().getAssets(), Konstanten.FONT_BOLD);
         latoFontLight = Tools.getFont(parent.getContext().getAssets(), Konstanten.FONT_LIGHT);
 
         // create a new view
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.systemlist_item, parent, false);
+        final View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.systemlist_item, parent, false);
         v.setDrawingCacheEnabled(true);
 
-        ViewHolder vh = new ViewHolder(v);
+        ViewHolder vh = new ViewHolder(v, new SystemsRecycleListViewAdapter.IMyViewHolderClicks() {
+            @Override
+            public void onSystemClick(SystemsRecycleListViewAdapter.ViewHolder caller) {
+
+                if (Reachability.reachability.isReachable) {
+                    CheatDatabaseApplication.getEventBus().post(new SystemListRecyclerViewClickEvent(mSystemObjects.get(caller.getAdapterPosition())));
+                } else {
+                    CheatDatabaseApplication.getEventBus().post(new SystemListRecyclerViewClickEvent(new Exception()));
+                }
+            }
+        });
+
+
         return vh;
     }
-
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
@@ -70,8 +101,13 @@ public class SystemsRecycleListViewAdapter extends RecyclerView.Adapter<SystemsR
         holder.mSystemName.setTypeface(latoFontBold);
 
         try {
-            holder.mSubtitle.setText(systemObj.getGameCount() + " Games");
-            holder.mSubtitle.setTypeface(latoFontLight);
+            if (systemObj.getGameCount() > 0) {
+                holder.mSubtitle.setVisibility(View.VISIBLE);
+                holder.mSubtitle.setText(systemObj.getGameCount() + " Games");
+                holder.mSubtitle.setTypeface(latoFontLight);
+            } else {
+                holder.mSubtitle.setVisibility(View.GONE);
+            }
         } catch (Exception e) {
             // Do nothing
         }
@@ -83,4 +119,5 @@ public class SystemsRecycleListViewAdapter extends RecyclerView.Adapter<SystemsR
     public int getItemCount() {
         return mSystemObjects.size();
     }
+
 }
