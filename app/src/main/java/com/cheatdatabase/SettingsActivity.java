b7109abf04566411ffde430100f2faf720d44bde
taar1 <taar1@d2b1d87f-eb27-4b14-ba05-7084dce0a13f>
2017-12-14 21:08:28 +0000

    Settings View fertig gemacht mit Switch für Achievements ausblenden

--- app/src/main/java/com/cheatdatabase/SettingsActivity.java
+++ app/src/main/java/com/cheatdatabase/SettingsActivity.java
@@ -30,24 +30,33 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     MyPrefs_ myPrefs;
 
     // DISABLE ACHIEVEMENTS
-    @PreferenceByKey(R.string.pref_diable_achievements_key)
-    SwitchPreference disableAchievements;
+    @PreferenceByKey(R.string.pref_enable_achievements_key)
+    SwitchPreference enableAchievements;
 
     // Called after changing the switch value
-    @PreferenceClick(R.string.pref_diable_achievements_key)
+    @PreferenceClick(R.string.pref_enable_achievements_key)
     void prefClick(SwitchPreference preference) {
-        boolean isAchievementsDisabled = preference.isChecked();
-        Log.d(TAG, "setDisableAchievements to: " + isAchievementsDisabled);
-        if (isAchievementsDisabled) {
-            disableAchievements.setSummary(R.string.pref_achievements_button_enabled);
+        boolean isAchievementsEnabled = preference.isChecked();
+        Log.d(TAG, "setEnableAchievements to: " + isAchievementsEnabled);
+        if (isAchievementsEnabled) {
+            enableAchievements.setSummary(R.string.enabled);
         } else {
-            disableAchievements.setSummary(R.string.pref_achievements_button_disabled);
+            enableAchievements.setSummary(R.string.disabled);
         }
     }
 
     @AfterPreferences
     void initPrefs() {
         PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
+
+        if (myPrefs.isAchievementsDisabled().getOr(true)) {
+            enableAchievements.setSummary(R.string.enabled);
+            enableAchievements.setChecked(true);
+        } else {
+            enableAchievements.setSummary(R.string.disabled);
+            enableAchievements.setChecked(false);
+        }
+
         setupActionBar();
     }
 
