33c21354d58502a3f4f1b63c1ebce8e1978fa87b
Dominik <derbsland@gmail.com>
2019-08-09 16:14:57 +0200

    Crashlytics Bug Fixes
    Version 2.4-preRelease

--- app/src/main/java/com/cheatdatabase/SubmitCheatActivity.java
+++ app/src/main/java/com/cheatdatabase/SubmitCheatActivity.java
@@ -115,7 +115,6 @@ public class SubmitCheatActivity extends AppCompatActivity {
         }
     }
 
-
     private void init() {
         if (mToolbar != null) {
             setSupportActionBar(mToolbar);
