53a397b87f9ac1d232c73f38474cff1b4448a280
Dominik <derbsland@gmail.com>
2019-09-18 08:59:04 +0200

    Bug Fix

--- app/src/main/java/com/cheatdatabase/fragments/SystemListFragment.java
+++ app/src/main/java/com/cheatdatabase/fragments/SystemListFragment.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -144,7 +143,6 @@ public class SystemListFragment extends Fragment implements OnSystemListItemSele
                     Tools.showSnackbar(outerLayout, getString(R.string.err_data_not_accessible));
                 } catch (NullPointerException | IllegalStateException e) {
                     Log.e(TAG, "NullPointerException or IllegalStateException: " + e.getLocalizedMessage());
-                    Toast.makeText(getActivity(), getString(R.string.err_data_not_accessible), Toast.LENGTH_LONG).show();
                 }
             }
         });
