5b9e2ace7ecd34238cef70b8f79d0503aab85a43
Dominik <derbsland@gmail.com>
2020-09-26 22:22:35 +0200

    Adding first working JUnit4 Test: MainActivityMTest.java

---
+++ app/src/androidTest/java/com/cheatdatabase/activity/MainActivityMTest.java
@@ -0,0 +1,23 @@
+package com.cheatdatabase.activity;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainActivityMTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testMe() {
+        assertEquals("hhhhh", "hhhhh");
+    }
+}
