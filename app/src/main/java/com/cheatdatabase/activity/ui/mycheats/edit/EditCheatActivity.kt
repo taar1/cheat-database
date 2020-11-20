0b5d1a27848709a595228263d0f060dc937b5f66
Dominik <derbsland@gmail.com>
2020-11-20 21:03:15 +0100

    Added DistinctValues.java for Huawei flavor

--- app/src/main/java/com/cheatdatabase/activity/ui/mycheats/edit/EditCheatActivity.kt
+++ app/src/main/java/com/cheatdatabase/activity/ui/mycheats/edit/EditCheatActivity.kt
@@ -1,12 +1,10 @@
 package com.cheatdatabase.activity.ui.mycheats.edit
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
 import com.cheatdatabase.R
-import com.cheatdatabase.data.model.Cheat
 
 class EditCheatActivity : AppCompatActivity() {
     private val TAG = "EditCheatActivity"
@@ -20,15 +18,17 @@ class EditCheatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.edit_cheat_activity)
 
-        var cheat = intent.getParcelableExtra("cheat") as Cheat
-        Log.d(TAG, "XXXXX onCreate: " + cheat.cheatTitle)
+        // TODO get the intent
+//        var cheat = intent.getParcelableExtra("cheat") as Cheat
+//        Log.d(TAG, "XXXXX onCreate: " + cheat.cheatTitle)
+//
+//        toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//
+//        supportActionBar?.title = cheat.gameName
+//        supportActionBar?.subtitle = cheat.cheatTitle
 
-        toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        supportActionBar?.title = cheat.gameName
-        supportActionBar?.subtitle = cheat.cheatTitle
 
 //        centeredText = findViewById(R.id.message)
 //        centeredText.text = "HALLO"
